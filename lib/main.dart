03ffaa0c44054bafc50d6f7026fcbd5362870c5a
aresm1999 <auliarizki0970@gmail.com>
2022-03-19 08:42:10 +0700

    Praktikum 5

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:navigationroute/pages/home_page.dart';
+import 'package:navigationroute/pages/item_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+      '/item': (context) => const ItemPage(),
+    },
+  ));
+}
